d7b6818fdf946a82f82359dc0fa7107888a8f5d0
Balon2103 <brea820@gmail.com>
2026-01-31 11:29:38 -0400

    .en2

--- servidor.js
+++ servidor.js
@@ -56,7 +56,7 @@ app.get("/", (req, res) => {
 });
 
 // --- LANZAMIENTO ---
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en: http://localhost:${PORT}`);
   console.log(
